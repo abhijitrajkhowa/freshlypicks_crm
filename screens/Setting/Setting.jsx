19de90608ec64d6c1f017bff4cc4d95ebe0eee52
Alok Saikia <aloksaikia94@gmail.com>
2024-01-18 17:59:28 +0530

    the theme setting is now persisted even when the user closes the app

--- screens/Setting/Setting.jsx
+++ screens/Setting/Setting.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Setting.module.css';
-import { useState } from 'react';
-import { Radio, Space, Input } from 'antd';
-import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { Radio, Space, Input, Divider } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 import { SET_LIGHT_THEME, SET_DARK_THEME } from '../../redux/types';
 
 const Setting = () => {
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState(0);
+  const customTheme = useSelector((state) => state.themeReducer);
+
   const dispatch = useDispatch();
 
   const onChange = (e) => {
@@ -15,27 +17,54 @@ const Setting = () => {
     switch (e.target.value) {
       case 1:
         dispatch({ type: SET_LIGHT_THEME });
+        window.electron.invoke('set-store-value', 'theme', 'light');
         break;
       case 2:
         dispatch({ type: SET_DARK_THEME });
+        window.electron.invoke('set-store-value', 'theme', 'dark');
         break;
       default:
         break;
     }
   };
 
+  useEffect(() => {
+    const storedTheme = window.localStorage.getItem('theme');
+    if (storedTheme === 'light') {
+      dispatch({
+        type: SET_LIGHT_THEME,
+      });
+    } else if (storedTheme === 'dark') {
+      dispatch({
+        type: SET_DARK_THEME,
+      });
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (customTheme === 'light') {
+      setValue(1);
+    } else {
+      setValue(2);
+    }
+  }, [customTheme]);
+
   return (
     <>
       <div className={styles.setting}>
-        <div className="mainContents">
-          <div className="customizationSection">
+        <div className={styles.mainContents}>
+          <div className={styles.customizationSection}>
             <h2>Customization</h2>
-            <Radio.Group onChange={onChange} value={value}>
-              <Space direction="vertical">
-                <Radio value={1}>Light Mode</Radio>
-                <Radio value={2}>Dark Mode</Radio>
-              </Space>
-            </Radio.Group>
+            <Divider />
+            <div className={styles.themeHeaderAndContentsWrapper}>
+              <h3>Theme</h3>
+              <Radio.Group onChange={onChange} value={value}>
+                <Space direction="vertical">
+                  <Radio value={1}>Light Mode</Radio>
+                  <Radio value={2}>Dark Mode</Radio>
+                </Space>
+              </Radio.Group>
+            </div>
           </div>
         </div>
       </div>
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,14 +5,33 @@ import { ConfigProvider, theme } from 'antd';
 import { RefineThemes } from '@refinedev/antd';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Home from '../../screens/Home/Home';
 import Login from '../../screens/Login/Login';
+import { SET_DARK_THEME, SET_LIGHT_THEME } from '../../redux/types';
 
 export default function App() {
   const customTheme = useSelector((state: any) => state.themeReducer);
+  const dispatch = useDispatch();
 
+  // Set theme from local storage on load
   useEffect(() => {
+    const storedTheme = window.localStorage.getItem('theme');
+    if (storedTheme === 'light') {
+      dispatch({
+        type: SET_LIGHT_THEME,
+      });
+    } else if (storedTheme === 'dark') {
+      dispatch({
+        type: SET_DARK_THEME,
+      });
+    }
+  }, [dispatch]);
+
+  // Update theme in local storage and document body when it changes
+  useEffect(() => {
+    window.localStorage.setItem('theme', customTheme);
     document.body.style.setProperty(
       'background-color',
       customTheme === 'dark' ? '#2C2C2C' : '#fafafa',
