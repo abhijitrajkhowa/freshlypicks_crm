2f0ef1cc9de5d3a020dfbae9031ec5e2fdcb9c57
Alok Saikia <aloksaikia94@gmail.com>
2024-01-18 12:40:17 +0530

    added the float button in the book keeping screen but is not fully functional yet

--- components/HorizontalNavBar/HorizontalNavBar.jsx
+++ components/HorizontalNavBar/HorizontalNavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './HorizontalNavBar.module.css';
 import { UserOutlined, SearchOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
@@ -6,16 +7,30 @@ import { useSelector } from 'react-redux';
 import { Input, Avatar, Popover } from 'antd';
 
 const HorizontalNavBar = () => {
+  const colorOptions = [
+    { backgroundColor: '#fde3cf', color: '#f56a00' },
+    { backgroundColor: '#c1e1c5', color: '#007f5f' },
+    { backgroundColor: '#ffd6a5', color: '#f07167' },
+    { backgroundColor: '#f0f3bd', color: '#114b5f' },
+    { backgroundColor: '#c5dedd', color: '#1a936f' },
+    { backgroundColor: '#e2cfea', color: '#4b3f72' },
+    { backgroundColor: '#ff9b54', color: '#563f1b' },
+    { backgroundColor: '#d0f4de', color: '#3c6e71' },
+    { backgroundColor: '#f6f7d7', color: '#3c493f' },
+    { backgroundColor: '#f4acb7', color: '#9a348e' },
+    { backgroundColor: '#9ef4e6', color: '#562349' },
+    { backgroundColor: '#f9cb9c', color: '#f46842' },
+    { backgroundColor: '#d2e1dd', color: '#414288' },
+    { backgroundColor: '#e3eaa7', color: '#0e9aa7' },
+    { backgroundColor: '#ffecd1', color: '#cb8fa9' },
+  ];
+
   const user = useSelector((state) => state.user);
+  const [avatarStyle, setAvatarStyle] = useState(colorOptions[0]);
 
   const inputStyle = {
     width: 300,
     fontFamily: 'Space Grotesk',
-  };
-
-  const avatarStyle = {
-    backgroundColor: '#fde3cf',
-    color: '#f56a00',
   };
 
   const popoverContent = (
@@ -24,6 +39,11 @@ const HorizontalNavBar = () => {
       <div>Phone number: {user.phone}</div>
     </div>
   );
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * colorOptions.length);
+    setAvatarStyle(colorOptions[randomIndex]);
+  }, []);
 
   return (
     <>
--- screens/BookKeeping/BookKeeping.jsx
+++ screens/BookKeeping/BookKeeping.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './BookKeeping.module.css';
+import { CommentOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+
+import { FloatButton } from 'antd';
 
 const BookKeeping = () => {
   return (
     <>
-      <div>BookKeeping</div>
+      <div>
+        <FloatButton.Group
+          trigger="click"
+          type="primary"
+          style={{ right: 48 }}
+          icon={<PlusOutlined />}
+        >
+          <FloatButton icon={<CommentOutlined />} />
+          <FloatButton icon={<EditOutlined />} />
+        </FloatButton.Group>
+      </div>
     </>
   );
 };
