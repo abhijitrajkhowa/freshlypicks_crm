f7ea1bdac87385a3a4bcb71e060ffd372401f0fd
Alok Saikia <aloksaikia94@gmail.com>
2024-02-02 10:16:15 +0530

    now the vertical nav can be collapsed

--- utils/helper.js
+++ utils/helper.js
@@ -1,11 +1,11 @@
-export const host = '192.168.1.2';
+export const host = '192.168.1.4';
 export const port = '8855';
 
 export const cloudinaryBaseUrl = 'https://api.cloudinary.com/v1_1/freshlypick';
 
-export const baseUrl = `https://freshlypicks.com/api`;
+// export const baseUrl = `https://freshlypicks.com/api`;
 // export const baseUrl = `https://freshlypicks.onrender.com`;
-// export const baseUrl = `http://${host}:${port}`;
+export const baseUrl = `http://${host}:${port}`;
 
 // command to generate index.android.bundle ---> 1st delete the file then run the command below then delete the drawable folders
 // npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
