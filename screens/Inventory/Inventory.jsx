7f5db533b76e975036b24ba0f8cef93adc11912c
Alok Saikia <aloksaikia94@gmail.com>
2024-02-03 19:22:57 +0530

    worked on the inventory screen features , now the user can add an online item to the inventory onlineItemsInventory collection

--- screens/Inventory/Inventory.jsx
+++ screens/Inventory/Inventory.jsx
@@ -42,17 +42,159 @@ import {
 } from 'antd';
 
 const Inventory = () => {
+  const products = useSelector((state) => state.products);
   const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
   const [isInitialLoading, setIsInitialLoading] = useState(false);
   const [isReloadButtonLoading, setIsReloadButtonLoading] = useState(false);
   const [searchedTerm, setSearchedTerm] = useState('');
+  const [
+    isAddOnlineItemToInventoryModalVisible,
+    setIsAddOnlineItemToInventoryModalVisible,
+  ] = useState(false);
+  const [selectedOnlineItem, setSelectedOnlineItem] = useState({});
+  const [isCreatingOnlineInventoryItem, setIsCreatingOnlineInventoryItem] =
+    useState(false);
+
+  const selectStyle = {
+    width: '100%',
+    margin: '16px 0 16px 0',
+  };
 
   const onDateChange = (date, dateString) => {
     setDate(dateString);
   };
 
+  const onSelectChange = (value) => {
+    if (value) {
+      products.map((product) => {
+        if (product.name === value) {
+          setSelectedOnlineItem(product);
+        }
+      });
+    } else {
+      setSelectedOnlineItem({});
+    }
+  };
+
+  const createOnlineInventoryItem = () => {
+    setIsCreatingOnlineInventoryItem(true);
+    window.electron
+      .invoke('api-request', {
+        method: 'POST',
+        url: `${baseUrl}/crm/create-online-inventory-item`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          name: selectedOnlineItem.name,
+          category: selectedOnlineItem.category,
+          initialStock: selectedOnlineItem.initialStock,
+          currentStock: selectedOnlineItem.currentStock,
+        },
+      })
+      .then((response) => {
+        const data = JSON.parse(response.body);
+        if (response.status !== 200) {
+          toast.error(data.error, {
+            position: 'bottom-center',
+          });
+          setIsCreatingOnlineInventoryItem(false);
+          setIsAddOnlineItemToInventoryModalVisible(false);
+          return;
+        }
+        toast.success(data.message, {
+          position: 'bottom-center',
+        });
+        setSelectedOnlineItem({});
+        setIsCreatingOnlineInventoryItem(false);
+        setIsAddOnlineItemToInventoryModalVisible(false);
+      })
+      .catch((err) => {
+        toast.error(err.message, {
+          position: 'bottom-center',
+        });
+        setIsCreatingOnlineInventoryItem(false);
+        setIsAddOnlineItemToInventoryModalVisible(false);
+      });
+  };
+
+  const processOrdersForSelect = () => {
+    const processedOrders = [];
+    products.forEach((product) => {
+      processedOrders.push(
+        <Select.Option key={product.name} value={product.name}>
+          {product.name}
+        </Select.Option>,
+      );
+    });
+    return processedOrders;
+  };
+
   return (
     <>
+      <Modal
+        centered
+        title="Add online item to inventory"
+        open={isAddOnlineItemToInventoryModalVisible}
+        onCancel={() => {
+          setIsAddOnlineItemToInventoryModalVisible(false);
+          setSelectedOnlineItem({});
+        }}
+        onOk={() => {
+          createOnlineInventoryItem();
+        }}
+        okButtonProps={{
+          disabled: Object.keys(selectedOnlineItem).length === 0,
+          loading: isCreatingOnlineInventoryItem,
+        }}
+      >
+        <Select
+          onChange={(value) => onSelectChange(value)}
+          size="large"
+          allowClear
+          showSearch
+          value={
+            Object.keys(selectedOnlineItem).length > 0
+              ? selectedOnlineItem.name
+              : 'Select a product'
+          }
+          style={selectStyle}
+        >
+          {processOrdersForSelect()}
+        </Select>
+        {Object.keys(selectedOnlineItem).length > 0 && (
+          <Form>
+            <Form.Item label="Product name">
+              <Input value={selectedOnlineItem.name} disabled />
+            </Form.Item>
+            <Form.Item label="Product category">
+              <Input value={selectedOnlineItem.category} disabled />
+            </Form.Item>
+            <Form.Item label="Initial stock">
+              <Input
+                value={selectedOnlineItem.initialStock?.count}
+                onChange={(e) => {
+                  setSelectedOnlineItem({
+                    ...selectedOnlineItem,
+                    initialStock: [
+                      {
+                        date: new Date(date),
+                        quantity: parseInt(e.target.value),
+                      },
+                    ],
+                    currentStock: [
+                      {
+                        date: new Date(date),
+                        quantity: parseInt(e.target.value),
+                      },
+                    ],
+                  });
+                }}
+              />
+            </Form.Item>
+          </Form>
+        )}
+      </Modal>
       <div className={styles.inventory}>
         <div className={styles.mainContents}>
           <div className={styles.datePickerWrapper}>
@@ -79,7 +221,18 @@ const Inventory = () => {
               value={searchedTerm}
               onChange={(e) => setSearchedTerm(e.target.value)}
             />
+            <Button
+              onClick={() => {
+                setIsAddOnlineItemToInventoryModalVisible(true);
+              }}
+              icon={<PlusOutlined />}
+              type="primary"
+              size="large"
+            >
+              Add online item to inventory
+            </Button>
           </div>
+          <div className={styles.OnlineInventoryItemsWrapper}></div>
         </div>
       </div>
     </>
